9be8f2d820b7d8edd2bc96eb04c3c5e0003a7f1d
commandblock2 <tony_hu@live.com>
2023-06-26 21:59:40 +0800

    just any login form

--- a-svelte-kit-project/src/lib/components/sign-in-form.svelte
+++ a-svelte-kit-project/src/lib/components/sign-in-form.svelte
@@ -3,14 +3,38 @@
     let password = "";
 </script>
 
-<form method="POST" class="space-y-5 {$$props.class} flex flex-col">
-    <input id="email" name="email" type="email" bind:value={email} required />
-    <input
-        id="password"
-        name="password"
-        type="password"
-        bind:value={password}
-        required
-    />
-    <button type="submit">Sign In</button>
+<form method="post" class="flex-col border-2 border-slate-500 m-28">
+    <div class="form-element">
+        <label for="input1">Email</label>
+        <input
+            type="text"
+            id="input1"
+            placeholder="Input 1"
+            class="input-field"
+            bind:value={email}
+        />
+    </div>
+
+    <div class="form-element">
+        <label for="input2">Password</label>
+        <input
+            type="text"
+            id="input2"
+            placeholder="Input 2"
+            class="input-field"
+            bind:value={password}
+        />
+    </div>
+
+    <button class="border-2 w-auto px-4 py-2 justify-self-center">Sign In</button>
 </form>
+
+<style>
+    .form-element {
+        @apply flex mb-2 border-b-2 pb-2 space-x-2;
+    }
+
+    .input-field {
+        @apply border-2 border-gray-300 mb-4;
+    }
+</style>
