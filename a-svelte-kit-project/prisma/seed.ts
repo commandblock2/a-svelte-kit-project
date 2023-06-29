05cef0d9a59a028a13505f82cc7c3f266fa9cbdc
commandblock2 <tony_hu@live.com>
2023-06-29 16:58:18 +0800

    updated schema and seeding process

--- a-svelte-kit-project/prisma/seed.ts
+++ a-svelte-kit-project/prisma/seed.ts
@@ -1,7 +1,7 @@
 // prisma/seed.ts
 
-import { BallQuality, PrismaClient } from '@prisma/client'
-import testData from "../src/lib/data.json" assert { type: "json" }
+import { BallQuality, PrismaClient, TransactionType } from '@prisma/client'
+import testData from "../src/lib/test-data.json" assert { type: "json" }
 
 const prisma = new PrismaClient()
 
@@ -16,7 +16,7 @@ async function main() {
                 phoneNumber: userData['phone-number'],
                 name: userData.name
             }
-        })   
+        })
     }
 
     for (const ballPriceData of testData['ball-price']) {
@@ -27,6 +27,57 @@ async function main() {
             }
         })
     }
+
+    for (const locationData of testData.places) {
+        const location = await prisma.location.create({
+            data: {
+                name: locationData.name,
+                longitude: locationData.longitude,
+                latitude: locationData.latitude
+            }
+        })
+    }
+
+    for (const machineData of testData.machines) {
+        const location = await prisma.location.findFirst()
+        const machine = await prisma.machine.create({
+            data: {
+                preciseLatitude: machineData['precise-latitude'],
+                preciseLongitude: machineData['precise-longitude'],
+                location: { connect: { id: location!.id } },
+                ballCountByQuality: {
+                    createMany: {
+                        data: Object.entries(machineData.balls).map(([quality, count]) => ({
+                            ballQuality: quality as BallQuality,
+                            count: count,
+                        }))
+                    }
+                }
+            }
+        })
+    }
+
+    for (const transactionData of testData.transactions) {
+        const machine = await prisma.machine.findFirst();
+        const user = await prisma.user.findFirst();
+
+        const transaction = await prisma.transaction.create({
+            data: {
+                ballCountByQuality: {
+                    createMany: {
+                        data: Object.entries(transactionData.balls).map(([quality, count]) => ({
+                            ballQuality: quality as BallQuality,
+                            count: count,
+                        }))
+                    }
+                },
+                user: { connect: { id: user?.id } },
+                machine: { connect: { id: machine?.id } },
+                type: transactionData.type as TransactionType,
+            },
+        });
+    }
+
 
 
     console.log(`Seeding finished.`)
